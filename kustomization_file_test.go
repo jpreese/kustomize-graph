3452bd37329dcbd84d7c7e7f5fc14f8d4bc532ce
John Reese <john@reese.dev>
2019-05-22 10:21:27 -0400

    Add initial tests

---
+++ kustomization_file_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestGet(t *testing.T) {
+	file, _ := NewKustomizationFile().Get("testing/get/")
+
+	expected := "a"
+	actual := file.Resources[0]
+
+	if actual != expected {
+		t.Errorf("Get returned wrong value, got %s, want: %s", actual, expected)
+	}
+}
+
+func TestGetMissingResources(t *testing.T) {
+	file, _ := NewKustomizationFile().Get("testing/getmissingresources/")
+
+	actual, _ := file.GetMissingResources()
+	expected := []string{"a.yaml"}
+
+	if reflect.DeepEqual(actual, expected) == false {
+		t.Errorf("GetMissingResources returned wrong resources, got %s, want: %s", actual, expected)
+	}
+}
